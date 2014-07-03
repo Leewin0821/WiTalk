e880a237170dbd73663fd335403a6c964d2c2b33
Leewin0821 <leewin.zhang@gmail.com>
2014-07-03 21:59:46 +0100

    click avatar to start search peers

---
+++ AndroidStudioProjects/WiTalk/app/src/main/java/leewin/macs/uk/ac/hw/witalk/People.java
@@ -0,0 +1,29 @@
+package leewin.macs.uk.ac.hw.witalk;
+
+/**
+ * Created by leewin on 14-7-3.
+ */
+
+
+public class People {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
